cfab7b1b98e4eb772c6c4e0e780fcff7f3b8a916
CreepSplotion <dekaiyondavis8@gmail.com>
2025-11-12 00:19:41 -0500

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,17 @@
+finalList = []
+yourList = []
+ifContinue = 'yes'
+
+while ifContinue == 'yes':
+  yourList.append(input("What word do you want to add to your list?: "))
+  ifContinue = input("Do you want to add anymore words to the list?(yes/no): ")
+
+
 def mySlice(yourList, index1, index2):
-  numberOfItems = index2-index1
-  finalList = list.pop(index1, numberOfItems)
-  return finalList
-mySlice()
+    for item in range (index1, index2):
+        finalList.append(yourList[item])
+    return finalList
+
+startingIndex = int(input("What will be the starting index?(integer): "))
+endingIndex = int(input("What will be the ending index?(integer): "))
+print(mySlice(yourList, startingIndex, endingIndex))
